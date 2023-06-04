8d7a4d1dd92c755db276ac3580f82252b84a8d3a
Syric <syric28@gmail.com>
2023-06-04 11:41:14 -0400

    Removed unnecessary import

--- src/main/java/com/tetranomicon/tetranomicon/TierRegistration.java
+++ src/main/java/com/tetranomicon/tetranomicon/TierRegistration.java
@@ -9,7 +9,6 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.common.TierSortingRegistry;
-import se.mickelus.tetra.TetraRegistries;
 
 import java.util.List;
 
