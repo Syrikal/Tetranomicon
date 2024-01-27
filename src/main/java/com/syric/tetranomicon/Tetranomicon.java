14da4562b502fc4e4449e9ce2c5105376a40450f
Syric <syric28@gmail.com>
2024-01-27 18:54:31 -0500

    Added debugging capabilities

--- src/main/java/com/syric/tetranomicon/Tetranomicon.java
+++ src/main/java/com/syric/tetranomicon/Tetranomicon.java
@@ -7,10 +7,13 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Tetranomicon.MODID)
 public class Tetranomicon {
     public static final String MODID = "tetranomicon";
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public Tetranomicon () {
         MinecraftForge.EVENT_BUS.register(this);
@@ -20,5 +23,10 @@ public class Tetranomicon {
         if (ModList.get().isLoaded("oresabovediamonds")) {
             TetranomiconItems.ITEMS.register(modEventBus);
         }
+
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+        forgeEventBus.register(new Debugging());
+
+
     }
 }
