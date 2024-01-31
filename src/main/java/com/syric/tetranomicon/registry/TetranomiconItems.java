920e9c116e0b58f47443b3ee2b3dcae788c24cba
Syric <syric28@gmail.com>
2024-01-31 18:13:21 -0500

    Added other mods to 1.16 testing

--- src/main/java/com/syric/tetranomicon/registry/TetranomiconItems.java
+++ src/main/java/com/syric/tetranomicon/registry/TetranomiconItems.java
@@ -2,15 +2,14 @@ package com.syric.tetranomicon.registry;
 
 import com.syric.tetranomicon.Tetranomicon;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.fml.RegistryObject;
+import oresAboveDiamonds.init.ModItemGroups;
 
 
 public class TetranomiconItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tetranomicon.MODID);
-//    public static final RegistryObject<Item> NETHERITE_OPAL = ITEMS.register("netherite_opal", () -> new Item(new Item.Properties().tab(ModItemGroups.OAD_ITEM_GROUP)));
-    public static final RegistryObject<Item> NETHERITE_OPAL = ITEMS.register("netherite_opal", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> NETHERITE_OPAL = ITEMS.register("netherite_opal", () -> new Item(new Item.Properties().tab(ModItemGroups.OAD_ITEM_GROUP)));
 
 }
