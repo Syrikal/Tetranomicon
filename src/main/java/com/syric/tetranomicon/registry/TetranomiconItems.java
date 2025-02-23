0afe38bd23750a26504b5fb7722a6ae8f0084262
Syric <syric28@gmail.com>
2025-02-22 20:26:28 -0500

    Draft 1.6 Update for 1.20

--- src/main/java/com/syric/tetranomicon/registry/TetranomiconItems.java
+++ src/main/java/com/syric/tetranomicon/registry/TetranomiconItems.java
@@ -20,6 +20,8 @@ import oresAboveDiamonds.init.ModItems;
 public class TetranomiconItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tetranomicon.MODID);
     public static final RegistryObject<Item> NETHERITE_OPAL = ITEMS.register("netherite_opal", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CINCINNASITE_DIAMOND = ITEMS.register("cincinnasite_diamond", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FLAMING_RUBY = ITEMS.register("flaming_ruby", () -> new Item(new Item.Properties()));
 
     public TetranomiconItems() {
     }
@@ -27,11 +29,17 @@ public class TetranomiconItems {
     @SubscribeEvent
     public static void buildCreativeModeTabs(BuildCreativeModeTabContentsEvent event) {
         ResourceKey<CreativeModeTab> tab = event.getTabKey();
+//        Tetranomicon.LOGGER.debug("Tab resourceLocation: " + tab.toString() + ", " + tab.location() + ", " + tab.location().getPath());
         if (ModList.get().isLoaded("oresabovediamonds")) {
             if (tab == ModItemGroups.OAD_TAB.getKey()) {
                 event.getEntries().putBefore(new ItemStack(ModItems.BLACK_OPAL.get()), new ItemStack(NETHERITE_OPAL.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
             }
         }
+        if (ModList.get().isLoaded("betternether")) {
+            if (tab.location().toString().equals("betternether:items_tab")) {
+                event.getEntries().put(new ItemStack(CINCINNASITE_DIAMOND.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.getEntries().put(new ItemStack(FLAMING_RUBY.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
+        }
     }
-
 }
